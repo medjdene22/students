243161065c24685e20306f17e087506b6fede4bf
imad <113237261+medjdene22@users.noreply.github.com>
2025-02-14 17:59:30 +0100

    first prod

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,6 +1,5 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { cors } from "hono/cors";
 
 import { auth } from '@/lib/auth';
 import { sessionMiddleware } from '@/lib/session-middleware';
@@ -16,17 +15,6 @@ const routes = app
     .on(["POST", "GET"], "/auth/*", (c) => {
         return auth.handler(c.req.raw);
     })
-    .use(
-        "/api/auth/*", // or replace with "*" to enable cors for all routes
-        cors({
-            origin: "http://localhost:3001", // replace with your origin
-            allowHeaders: ["Content-Type", "Authorization"],
-            allowMethods: ["POST", "GET", "OPTIONS"],
-            exposeHeaders: ["Content-Length"],
-            maxAge: 600,
-            credentials: true,
-        }),
-    )
     .route("/authentication", authentication)
     .route("/major", major)
     .route("/specialtie", specialtie)
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,6 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
 
-export const db = drizzle({connection: process.env.DATABASE_URL!, casing: 'snake_case', schema: {...schema} });
+const sql = neon(process.env.NEON_DATABASE_URL!);
+export const db = drizzle({ client: sql, schema: { ...schema} })
