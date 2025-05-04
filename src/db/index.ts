82cc634d0de77707de225f73d371364a648aeb4d
imad <113237261+medjdene22@users.noreply.github.com>
2025-05-04 20:15:51 +0100

    olmost 3

---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import * as schema from './schema'
+
+export const db = drizzle({connection: process.env.DATABASE_URL!, casing: 'snake_case', schema: {...schema} });
+
+// import { neon } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-http';
+// import * as schema from './schema'
+
+// const sql = neon(process.env.NEON_DATABASE_URL!);
+// export const db = drizzle({ client: sql, schema: { ...schema} })
